f7b3cce3c6af7aab3e59626876ba322c35c05552
xuantho.do <xuantho.do@citynow.vn>
2021-11-08 13:18:19 +0700

    add model.c file

--- audio_recording/record.py
+++ audio_recording/record.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
+import sys
 
-import argparse
 
 fs = 16000  # Sample rate
 seconds = 1  # Duration of recording
@@ -9,4 +9,4 @@ print("Start Recording")
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, dtype='int16', channels=1)
 sd.wait()  # Wait until recording is finished
 print("Stop Recording")
-write('output.wav', fs, myrecording)  # Save as WAV file
+write('./{word}/output.wav'.format(word = sys.argv[1]), fs, myrecording)  # Save as WAV file
